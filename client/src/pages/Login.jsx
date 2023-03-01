9affd34b42232e303392134ad140a4088e3f82d2
Aditya Maurya <76245095+aditya12data@users.noreply.github.com>
2023-03-01 22:56:27 +0530

    feat: Made UI of Register and login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import React, { useState } from "react";
+import {Link} from "react-router-dom"
+import "./style.css"
+function Login() {
+  const [name, setName] = useState("");
+  const [headingText, setHeading] = useState("");
 
-const Login = () => {
+  function handleChange(event) {
+    console.log(event.target.value);
+    setName(event.target.value);
+  }
+
+  function handleClick(event) {
+    setHeading(name);
+
+    event.preventDefault();
+  }
+
   return (
-    <div>
-      <>Login</>
+    <div className="container">
+      <h1>Welcome !!{headingText}</h1>
+      <form onSubmit={handleClick}>
+        <input
+          onChange={handleChange}
+          type="text"
+          placeholder="What's your email?"
+          value={name}
+        />
+        <input
+          onChange={handleChange}
+          type="password"
+          placeholder="What's your password?"
+          value={name}
+        />
+        <button className="Button" type="submit">Submit</button>
+        <div>
+        <span>Already have an account?  <Link style={{color:"whitesmoke"}} to="/Register">Register</Link></span>
+        </div>
+      </form>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,11 +1,50 @@
-import React from 'react'
+import React, { useState } from "react";
+import {Link} from "react-router-dom"
+import "./style.css"
+function Register() {
+  const [name, setName] = useState("");
+  const [headingText, setHeading] = useState("");
 
-const Register = () => {
+  function handleChange(event) {
+    console.log(event.target.value);
+    setName(event.target.value);
+  }
+
+  function handleClick(event) {
+    setHeading(name);
+
+    event.preventDefault();
+  }
+
   return (
-    <div>
-      <>Register</>
+    <div className="container">
+      <h1>Create Account {headingText}</h1>
+      <form onSubmit={handleClick}>
+        <input
+          onChange={handleChange}
+          type="text"
+          placeholder="What's your name?"
+          value={name}
+        />
+        <input
+          onChange={handleChange}
+          type="text"
+          placeholder="What's your email?"
+          value={name}
+        />
+        <input
+          onChange={handleChange}
+          type="password"
+          placeholder="What's your password?"
+          value={name}
+        />
+        <button className="Button" type="Register">Submit</button>
+        <div>
+        <span>Already have an account?  <Link style={{color:"whitesmoke"}} to="/login">Login</Link></span>
+        </div>
+      </form>
     </div>
-  )
+  );
 }
 
-export default Register
+export default Register;
